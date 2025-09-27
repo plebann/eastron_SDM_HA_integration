b2460ab6868d0737f3ed9c99596789d7aa9ff650
plebann <m1pleban@gmail.com>
2025-09-27 18:49:45 +0200

    Fix

--- custom_components/eastron_sdm/client.py
+++ custom_components/eastron_sdm/client.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 from typing import Any
 
 from pymodbus.client import AsyncModbusTcpClient
-from pymodbus.framer.rtu_framer import ModbusRtuFramer
+from pymodbus.framer import ModbusRtuFramer
 from pymodbus.exceptions import ModbusIOException
 
 _LOGGER = logging.getLogger(__name__)
